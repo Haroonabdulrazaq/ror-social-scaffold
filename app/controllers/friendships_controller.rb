75d47878af0e9c6c11e70b26dd4d89c57d978f69
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-05 13:46:18 +0100

    Add Frienship to routes and add controllermethods

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,27 @@
 class FriendshipsController < ApplicationController
+    def create
+        @friendship = Friendship.create(user_id: current_user.id, friend_id: params[:friend_id], confirmed: true)
+        if @friendship.save
+          flash[:notice] = 'Added as a friend'
+        else
+          flash[:alert] = 'There was something wrong with the tracking request'
+        end
+        redirect_to root_path
+      end
+    
+      def update
+        Friendship.find(params[:id]).update(confirmed: true)
+        redirect_to root_path
+      end
+    
+      def destroy
+        Friendship.find(params[:id]).destroy
+        redirect_to root_path
+      end
+    
+      private
+    
+      def friendship_params
+        params.require(:friendship).permit(:confirmed)
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
     resources :comments, only: [:create]
     resources :likes, only: %i[create destroy]
   end
-
+  resources :friendships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
