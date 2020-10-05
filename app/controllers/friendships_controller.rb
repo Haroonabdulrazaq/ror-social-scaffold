941af3837513a1bf0da594f90eaf8ae182a95446
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-05 14:53:33 +0100

    Fix linters and rubocop err

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,27 +1,27 @@
 class FriendshipsController < ApplicationController
-    def create
-        @friendship = Friendship.create(user_id: current_user.id, friend_id: params[:friend_id], confirmed: true)
-        if @friendship.save
-          flash[:notice] = 'You are now friends'
-        else
-          flash[:alert] = 'Friend not added for some reasons'
-        end
-        redirect_to root_path
-      end
+  def create
+    @friendship = Friendship.create(user_id: current_user.id, friend_id: params[:friend_id], confirmed: true)
+    if @friendship.save
+      flash[:notice] = 'You are now friends'
+    else
+      flash[:alert] = 'Friend not added for some reasons'
+    end
+    redirect_to root_path
+  end
     
-      def update
-        Friendship.find(params[:id]).update(confirmed: true)
-        redirect_to root_path
-      end
-    
-      def destroy
-        Friendship.find(params[:id]).destroy
-        redirect_to root_path
-      end
-    
-      private
-    
-      def friendship_params
-        params.require(:friendship).permit(:confirmed)
-      end
+  def update
+    Friendship.find(params[:id]).update(confirmed: true)
+    redirect_to root_path
+  end
+
+  def destroy
+    Friendship.find(params[:id]).destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(:confirmed)
+  end
 end
--- spec/requests/friendships_request_spec.rb
+++ spec/requests/friendships_request_spec.rb
@@ -1,5 +1,3 @@
 require 'rails_helper'
-
-RSpec.describe "Friendships", type: :request do
-
+RSpec.describe 'Friendships', type: :request do
 end
