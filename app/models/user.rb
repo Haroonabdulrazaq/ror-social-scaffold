fa7645c378912ec00bc11f09a28684c20e7c4614
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-06 11:50:35 +0100

    Made changes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,17 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :friendships
-  has_many :friends, through: :friendships
-  has_many :accepted_friendships, -> { where confirmed: true }, class_name: 'Friendship'
-  has_many :accepted_friends, through: :accepted_friendships, source: :friend
+    # User model
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: "Friendship"
+  has_many :friends, through: :confirmed_friendships
+  # Users who requested to be friends (needed for notifications)
+  has_many :inverted_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :friend_requests, through: :inverted_friendships
+  # Users who needs to confirm friendship
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "user_id"
+  has_many :pending_friends, through: :pending_friendships, source: :friend
+
+  def friends_and_own_posts
+    Post.where(user: (self.friends + self))
+  end
 end
