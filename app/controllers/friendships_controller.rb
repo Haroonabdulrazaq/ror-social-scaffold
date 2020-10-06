81ff7abc8d4b85846ad9f8bbb8a9632444a23ed8
Luciano Ilha Carbonell <ilha.dev.br@gmail.com>
2020-10-06 12:08:05 -0300

    Remove button after friend acceptance

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,16 +1,18 @@
 class FriendshipsController < ApplicationController
+
   def create
-    @friendship = Friendship.create(user_id: current_user.id, friend_id: params[:friend_id], confirmed: true)
+    @friendship = Friendship.create(user_id: current_user.id, friend_id: params[:friend_id], confirmed: false)
     if @friendship.save
-      flash[:notice] = 'You are now friends'
+      flash[:notice] = 'Request is sent succesfully'
     else
-      flash[:alert] = 'Friend not added for some reasons'
+      flash[:alert] = 'Request is not sent'
     end
     redirect_to root_path
   end
 
   def update
-    Friendship.find(params[:id]).update(confirmed: true)
+    Friendship.find(params[:id]).update(confirmed: false)
+    flash[:notice] = 'Now you are friends!'
     redirect_to root_path
   end
 
