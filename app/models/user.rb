d6fd38c9ff52e571b4433178e4c98443eb88d512
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-10-06 11:54:55 +0100

    Fix error

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,14 +9,14 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-    # User model
-  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: "Friendship"
+  # User model
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
   has_many :friends, through: :confirmed_friendships
   # Users who requested to be friends (needed for notifications)
-  has_many :inverted_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :inverted_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :friend_requests, through: :inverted_friendships
   # Users who needs to confirm friendship
-  has_many :pending_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "user_id"
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
   has_many :pending_friends, through: :pending_friendships, source: :friend
 
   def friends_and_own_posts
